7f0fd35f223fe1a02742804f8a04825eb4ae291b
Rishabh Verma <2000rishabhverma@gmail.com>
2022-09-24 21:40:55 +0530

    time

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function start() {
     function secondsTimer() {
         seconds = seconds - 1;
         document.getElementById('seconds').innerHTML = seconds;
-        if (seconds <= 56) {
-            if (minutes <= 24) {
+        if (seconds <= 0) {
+            if (minutes <= 0) {
                 clearInterval(minutes_interval);
                 clearInterval(seconds_interval);
                 bell.play();
